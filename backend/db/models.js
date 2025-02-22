dba3a2d4ca2e7320414fe36013590bc5c8584f4e
sidharth_sinhasane <sidharth.sinhasane@gmail.com>
2025-02-22 13:07:53 +0530

    schemas added

---
+++ backend/db/models.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const inventrySchema = new mongoose.Schema({
+    rfid: {
+        type: int,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: int,
+        required: true
+    },
+    quantity: {
+        type:int,
+        required: true
+    },
+    expirty_date: {
+        type: Date,
+        required: true
+    }
+    });
+const locationSchema = new mongoose.Schema({
+    rfid: {
+        type: int,
+        required: true,
+        unique: true
+    },
+    section : {
+        type: String,
+        required: true
+    }
+    });
+const pricingSchema = new mongoose.Schema({
+    rfid: {
+        type: int,
+        required: true,
+        unique: true,
+        list:[
+            {company: String, price: int}
+        ]
+    }
+    });
