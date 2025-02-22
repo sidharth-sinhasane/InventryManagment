4283bb3ed818203ae1ba7dfb8ae0659b0a7523b8
sidharth_sinhasane <sidharth.sinhasane@gmail.com>
2025-02-22 16:40:16 +0530

    inventry post rout done

--- backend/routs/inventry.js
+++ backend/routs/inventry.js
@@ -9,4 +9,19 @@ inventryRouter.get('/id',async (req,res)=>{
    
     res.send({message:"inside inventry id router"});
 });
+
+inventryRouter.post('/',async (req,res)=>{
+    try{
+        const allInventry= req.body;
+        for(let i=0;i<allInventry.length;i++){
+            const inventry = new Inventry(allInventry[i]);
+            await inventry.save();
+        }
+        res.status(200).send({message:"inventry added successfully"});
+    }
+    catch(error){
+        console.log(error);
+        res.send({message:"error occured",error:error});
+    }
+});
 module.exports={inventryRouter};
