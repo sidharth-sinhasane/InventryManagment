4462114a1c7da1a85eb3de8b311b929fc00151a5
sample <anand.more_comp22@pccoer.in>
2025-02-22 16:22:51 +0530

    loactio rout updated

--- backend/routs/location.js
+++ backend/routs/location.js
@@ -2,8 +2,21 @@ const express = require('express');
 const LocationRouter=express.Router();
 const {Location}=require('../db/models');
 
-LocationRouter.get('/',async (req,res)=>{
-    res.send({message:"inside location router"});
+locationRouter.get("/", async (req, res) => {
+    try {
+        const locations = await Location.findAll();
+        res.json(locations);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching locations" });
+    }
 });
 
-module.exports={LocationRouter};
+locationRouter.get("/id", async (req, res) => {
+    try {
+        const Section = await location.findByPk(req.body.id);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching location" });
+    }
+});
+
+module.exports={locationRouter};
