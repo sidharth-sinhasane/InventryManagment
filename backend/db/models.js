7045fd03329a92d182a03e493d316526838c4564
sidharth_sinhasane <sidharth.sinhasane@gmail.com>
2025-02-22 14:43:43 +0530

    modules added

--- backend/db/models.js
+++ backend/db/models.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
-console.log(process.env.name)
+require('dotenv').config();
 const inventrySchema = new mongoose.Schema({
     rfid: {
         type: Number,
@@ -40,10 +39,19 @@ const pricingSchema = new mongoose.Schema({
         type: Number,
         required: true,
         unique: true,
-        list:[
-            {company: String, price: Number}
-        ]
-    }
+    },
+    list: [
+        {
+            company: {
+                type: String,
+                required: true
+            },
+            price: {
+                type: Number,
+                required: true
+            }
+        }
+    ]
     });
 const todaySalesSchema = new mongoose.Schema({
     rfid: {
@@ -62,7 +70,7 @@ const todaySalesSchema = new mongoose.Schema({
     });
     const connectToDatabase = async function (){
         try{
-            await mongoose.connect(process.env.MONGODB_URL)
+            await mongoose.connect("mongodb+srv://admin123:admin123@cluster0.ggizw.mongodb.net/")
             console.log("connected to database")
         }
         catch(error){
@@ -70,4 +78,9 @@ const todaySalesSchema = new mongoose.Schema({
             process.exit(1)
         }
     }
-    // connectToDatabase()
+connectToDatabase()
+const Inventry = mongoose.model('Inventry', inventrySchema);
+const Location = mongoose.model('Location', locationSchema);    
+const Pricing = mongoose.model('Pricing', pricingSchema);
+const TodaySales = mongoose.model('TodaySales', todaySalesSchema);
+module.exports = {Inventry,Location,Pricing,TodaySales,connectToDatabase};
