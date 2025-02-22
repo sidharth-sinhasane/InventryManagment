ede0057d52f89a702865b43eabc87e182995e7b5
sample <anand.more_comp22@pccoer.in>
2025-02-22 17:15:25 +0530

    inventory route updated

--- backend/routs/location.js
+++ backend/routs/location.js
@@ -2,16 +2,7 @@ const express = require('express');
 const locationRouter=express.Router();
 const {Location}=require('../db/models');
 
-locationRouter.get("/", async (req, res) => {
-    try {
-        const locations = await Location.findAll();
-        res.json(locations);
-    } catch (error) {
-        res.status(500).json({ error: "Error fetching locations" });
-    }
-});
-
-locationRouter.get('/',async (req,res)=>{  // ❌ Duplicate route
+locationRouter.get('/',async (req,res)=>{  /
     try {
         const locations = await Location.find({});
         res.json(locations);
